107f34620b233f8c9bdfbda73de7b8142e31de60
SamsonStannus <samsonstannus@gmail.com>
2015-10-14 18:32:17 -0400

    Added important stuff to README

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # Qotd
 
-###Quote of the day for your Terminal
-scraps quotes from Forbes quotes
+### Quote of the day for your Terminal
+<img src="images/qotd_screenshot.png" "style: width = 600px;">
+
+# How To:
+- requires nokogiri, mechanize, phantomjs, and watir
+- Place the qotd folder in your root directory, and run 
+```ruby bashStuff.rb``` (only run this once)
+
+- Then run the following everyday if you want a new quote
+```ruby qotd.rb```
+
--- qotd.rb
+++ qotd.rb
@@ -1,7 +1,8 @@
-require "nokogiri"
-require "mechanize"
+require 'nokogiri'
+require 'mechanize'
 require 'phantomjs'
 require 'watir'
+
 
 
 url = "http://www.forbes.com/quotes/"
@@ -15,33 +16,45 @@ doc = Nokogiri::HTML(b.html)
 
 list = doc.xpath("//blockquote[@class='bubble ng-scope']")
 
-list.each { |i| fp.write( "\""+(i.text.gsub('Share  F   ', "\"\n -")) +"\n") }
-
-File.open("../.bash_profile", "a"){ |f|
-	f<< "root=$( cd \"$( dirname \"${BASH_SOURCE[0]}\" )\" && pwd )
-
-line1=$(sed -n '3p' < $root/Quotes.txt)
-line2=$(sed -n '4p' < $root/Quotes.txt)
-
-size=$(tput cols)
-
-for i in $(seq 1 $size)
-do
-  	printf \"-\"
-done
-
-printf \"\n\"
-
-echo $line1
-echo $line2
-
-for i in $(seq 1 $size)
-do
-  	printf \"-\"
-done
-
-printf \"\n\""
+first = true
+list.each { |i| text = (i.text.gsub('Share  F   ', "\"\n -"))
+	if first == false
+		fp.write( "\""+ text.gsub(/([A-Z][a-z]*)[A-Z]/.match(text).captures.join(""), "") +"\n")
+	else
+		fp.write( "\""+ text +"\n")
+		first = false
+	end
+	
 }
+
+
+# .gsub(/([A-Z][a-z]*)[A-Z]/.match(i.text).captures.join(""), "")
+
+# File.open("../.bash_profile", "a"){ |f|
+# 	f<< "root=$( cd \"$( dirname \"${BASH_SOURCE[0]}\" )\" && pwd )
+
+# line1=$(sed -n '3p' < $root/Quotes.txt)
+# line2=$(sed -n '4p' < $root/Quotes.txt)
+
+# size=$(tput cols)
+
+# for i in $(seq 1 $size)
+# do
+#   	printf \"-\"
+# done
+
+# printf \"\n\"
+
+# echo $line1
+# echo $line2
+
+# for i in $(seq 1 $size)
+# do
+#   	printf \"-\"
+# done
+
+# printf \"\n\""
+# }
 
 
 
