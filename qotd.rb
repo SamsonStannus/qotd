b40adac778dde244c1a445c2fda7464e77a25203
SamsonStannus <samsonstannus@gmail.com>
2015-10-14 18:33:33 -0400

    deleted unneeded comments

--- qotd.rb
+++ qotd.rb
@@ -28,33 +28,6 @@ list.each { |i| text = (i.text.gsub('Share  F   ', "\"\n -"))
 }
 
 
-# .gsub(/([A-Z][a-z]*)[A-Z]/.match(i.text).captures.join(""), "")
-
-# File.open("../.bash_profile", "a"){ |f|
-# 	f<< "root=$( cd \"$( dirname \"${BASH_SOURCE[0]}\" )\" && pwd )
-
-# line1=$(sed -n '3p' < $root/Quotes.txt)
-# line2=$(sed -n '4p' < $root/Quotes.txt)
-
-# size=$(tput cols)
-
-# for i in $(seq 1 $size)
-# do
-#   	printf \"-\"
-# done
-
-# printf \"\n\"
-
-# echo $line1
-# echo $line2
-
-# for i in $(seq 1 $size)
-# do
-#   	printf \"-\"
-# done
-
-# printf \"\n\""
-# }
 
 
 
